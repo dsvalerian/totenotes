c0547fa24d78b638aa634087847c531a08f9de70
dsvalerian <dmitrisalov98@gmail.com>
2025-01-31 11:07:17 -0600

    initial frontend commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# totenotes
+# ToteNotes
 An app for creating and sharing grocery lists.
---
+++ frontend/src/app/App.tsx
@@ -0,0 +1,11 @@
+import styles from './App.module.css';
+
+const App = () => {
+  return (
+    <>
+      <p className={styles['text']}>Hello world</p>
+    </>
+  );
+};
+
+export default App;
